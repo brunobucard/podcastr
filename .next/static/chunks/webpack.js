d04ca8a264d9690bd8b4ddaea43b2657ac039cd0
Bruno Bucard <brunobucard@gmail.com>
2021-04-23 14:27:52 -0300

    Routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b31ca9e33341198463d8";
+/******/ 	var hotCurrentHash = "cbc563035564596282d4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ Aula03.md
@@ -0,0 +1,7 @@
+# Aula 03
+
+## Tipagem
+
+## Roteamento 1:00:00
+
+Como são definidas as rotas no react
